d044086d50a96b6c8f67bd32b9f6f8c32b0e6028
Andy Rudnitsky <a.rudnitsky@techsmith.com>
2012-11-17 18:10:50 -0500

    Card changes, added tests

--- Solitaire/Card.cs
+++ Solitaire/Card.cs
@@ -6,55 +6,42 @@ namespace Solitaire
 
    public enum CardColor { Red, Black };
 
+   public enum CardRank { Ace = 1, Two, Three, Four, Five, Six, Seven,
+                          Eight, Nine, Ten, Jack, Queen, King };
+
    public class Card
    {
       public CardSuit Suit { get; private set; }
-      public int Rank { get; private set; }
+      public CardRank Rank { get; private set; }
       public CardColor Color { get; private set; } 
 
       public Card( int rank, CardSuit suit )
       {
+         InitCard( (CardRank) rank, suit );
+      }
+      
+      public Card( CardRank rank, CardSuit suit )
+      {
+         InitCard( rank, suit );
+      }
+
+      private void InitCard( CardRank rank, CardSuit suit )
+      {
          Rank = rank;
          Suit = suit;
          if ( Suit == CardSuit.Hearts || Suit == CardSuit.Diamonds )
+         {
             Color = CardColor.Red;
+         }
          else
+         {
             Color = CardColor.Black;
+         }
       }
 
       public override string ToString()
       {
-         return string.Format("{0} of {1}", CardName(), Suit.ToString());
-      }
-
-      private string CardName()
-      {
-         string cardName;
-         if ( Rank > 1 && Rank < 11 )
-         {
-            cardName = Rank.ToString();
-         }
-         else
-         {
-            switch ( Rank )
-            {
-               case 11:
-                  cardName = "Jack";
-                  break;
-               case 12:
-                  cardName = "Queen";
-                  break;
-               case 13:
-                  cardName = "King";
-                  break;
-               case 1:
-                  cardName = "Ace";
-                  break;
-               default:
-                  throw new ArgumentException( "Rank is not 1 or > 10" );
-            }
-         }
-         return cardName;
+         return string.Format("{0} of {1}", Rank.ToString(), Suit.ToString());
       }
    }
 }
---
+++ SolitaireTest/CardTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solitaire;
+
+namespace SolitaireTest
+{
+   [TestClass]
+   public class CardTest
+   {
+      [TestMethod]
+      public void Card_ConstructCardVariousWays_CardIsCorrect()
+      {
+         var card1 = new Card( CardRank.Ace, CardSuit.Spades );
+         var card2 = new Card( 2, CardSuit.Hearts );
+         var card3 = new Card( CardRank.Three, CardSuit.Clubs );
+         var card4 = new Card( 4, CardSuit.Diamonds );
+
+         Assert.IsTrue( card1.Color == CardColor.Black );
+         Assert.IsTrue( card1.Rank == CardRank.Ace );
+         Assert.IsTrue( card1.Suit == CardSuit.Spades );
+
+         Assert.IsTrue( card2.Color == CardColor.Red );
+         Assert.IsTrue( card2.Rank == CardRank.Two );
+         Assert.IsTrue( card2.Suit == CardSuit.Hearts );
+
+         Assert.IsTrue( card3.Color == CardColor.Black );
+         Assert.IsTrue( card3.Rank == CardRank.Three );
+         Assert.IsTrue( card3.Suit == CardSuit.Clubs );
+
+         Assert.IsTrue( card4.Color == CardColor.Red );
+         Assert.IsTrue( card4.Rank == CardRank.Four );
+         Assert.IsTrue( card4.Suit == CardSuit.Diamonds );
+      }
+   }
+}
