990a3d30c603abc6db04c1e3c4a6ba20f3a56416
Andy Rudnitsky <a.rudnitsky@techsmith.com>
2012-11-17 20:24:10 -0500

    Added TableauPile and tests

---
+++ Solitaire/TableauPile.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solitaire
+{
+   public class TableauPile
+   {
+      private readonly Stack<Card> _cards;
+
+      public TableauPile()
+      {
+         _cards = new Stack<Card>();
+      }
+
+      public bool IsEmpty()
+      {
+         return _cards.Count == 0;
+      }
+
+      public void AddCard( Card card )
+      {
+         if ( card == null )
+         {
+            throw new ArgumentNullException();
+         }
+        
+         if ( CanAddCard( card ) )
+         {
+            _cards.Push( card );
+         }
+         else
+         {
+            throw new InvalidOperationException( "Can't add this card" );
+         }
+      }
+
+      public bool CanAddCard( Card card )
+      {
+         if ( card.Rank == CardRank.King && !IsEmpty() )
+         {
+            return false;
+         } 
+
+         if ( card.Rank != CardRank.King && IsEmpty() )
+         {
+            return false;
+         }
+
+         if ( !IsEmpty() )
+         {
+            if ( ! _cards.Peek().IsOppositeColor( card) )
+            {
+               return false;
+            }
+
+            if ( _cards.Peek().Rank < card.Rank )
+            {
+               return false;
+            }
+         }
+         
+         return true;
+      }
+
+      public Card RemoveCard()
+      {
+         if ( IsEmpty( ) )
+         {
+            throw new InvalidOperationException( "Pile is empty" );
+         }
+
+         return _cards.Pop();
+      }
+   }
+}
---
+++ SolitaireTest/TableauPileTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solitaire;
+
+namespace SolitaireTest
+{
+   [TestClass]
+   public class TableauPileTest
+   {
+      private TableauPile _pile;
+      private readonly Card _kingOfSpades;
+      private readonly Card _queenOfSpades;
+      private readonly Card _kingOfDiamonds;
+      private readonly Card _queenOfDiamonds;
+      private readonly Card _jackOfSpades;
+      private readonly Card _jackOfDiamonds;
+
+      public TableauPileTest()
+      {
+         _kingOfSpades = new Card( 13, CardSuit.Spades );
+         _queenOfSpades = new Card( 12, CardSuit.Spades );
+         _kingOfDiamonds = new Card( 13, CardSuit.Diamonds );
+         _queenOfDiamonds = new Card( 12, CardSuit.Diamonds );
+         _jackOfSpades = new Card( 11, CardSuit.Spades );
+         _jackOfDiamonds = new Card( 11, CardSuit.Diamonds );
+      }
+
+      [TestInitialize]
+      public void Setup()
+      {
+         _pile = new TableauPile();
+      }
+
+      [TestMethod]
+      public void Constructor_NewTableauPile_IsEmpty()
+      {
+         var pile = new TableauPile();
+         Assert.IsTrue( pile.IsEmpty() );
+      }
+
+      [TestMethod]
+      public void AddCard_NewPile_IsEmptyReturnsFalse()
+      {
+         _pile.AddCard( new Card( CardRank.King, CardSuit.Spades ) );
+         Assert.IsFalse( _pile.IsEmpty() );
+      }
+
+      [TestMethod]
+      [ExpectedException( typeof( ArgumentNullException ) )]
+      public void AddCard_CardIsNull_ThrowsArgumentNullException()
+      {
+         _pile.AddCard( null );
+         Assert.IsFalse( _pile.IsEmpty() );
+      }
+
+      [TestMethod]
+      public void CanAddCard_PileIsEmpty_CanAddKing()
+      {
+         Assert.IsTrue( _pile.CanAddCard( _kingOfSpades ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_PileIsEmpty_CanNotAddNonKing()
+      {
+         Assert.IsFalse( _pile.CanAddCard( _jackOfSpades ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_PileIsNotEmpty_CanNotAddKing()
+      {
+         _pile.AddCard( _kingOfSpades );
+         Assert.IsFalse( _pile.CanAddCard( _kingOfDiamonds ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_PileIsEmpty_CanNotAddCardOtherThanKing()
+      {
+         Assert.IsFalse( _pile.CanAddCard( new Card( CardRank.Queen, CardSuit.Spades ) ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_AddingCardOfSameColor_CanNotAdd()
+      {
+         _pile.AddCard( _kingOfDiamonds );
+         Assert.IsFalse( _pile.CanAddCard( _queenOfDiamonds ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_AddingCardOfDifferentColor_CanAdd()
+      {
+         _pile.AddCard( _kingOfDiamonds );
+         Assert.IsTrue( _pile.CanAddCard( _queenOfSpades ) );
+      }
+      
+      [TestMethod]
+      public void CanAddCard_AddingCardOfSameOrHigherRank_CanNotAdd()
+      {
+         _pile.AddCard( _kingOfDiamonds );
+         _pile.AddCard( _queenOfSpades );
+         _pile.AddCard( _jackOfDiamonds );
+         Assert.IsFalse( _pile.CanAddCard( _queenOfSpades ) );
+         Assert.IsFalse( _pile.CanAddCard( _kingOfSpades ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_AddingCardOfLowerRank_CanAdd()
+      {
+         _pile.AddCard( _kingOfDiamonds );
+         Assert.IsTrue( _pile.CanAddCard( _queenOfSpades ) );
+      }
+
+      [TestMethod]
+      [ExpectedException(typeof(InvalidOperationException))]
+      public void RemoveCard_PileIsEmpty_Throws_InvalidOperationException()
+      {
+         _pile.RemoveCard();
+      }
+
+      [TestMethod]
+      public void RemoveCard_RemoveCard_ReturnsLastCardAdded()
+      {
+         var expected = _kingOfDiamonds;
+         _pile.AddCard( expected );
+         var actual = _pile.RemoveCard();
+         Assert.AreEqual( actual, expected );
+      }
+   }
+}
