bbb4f3e69fb610642f1e31bc7680f8982461628e
Andy Rudnitsky <a.rudnitsky@techsmith.com>
2012-11-18 14:31:52 -0500

    Added FoundationPile and tests

---
+++ Solitaire/FoundationPile.cs
@@ -0,0 +1,33 @@
+﻿namespace Solitaire
+{
+   public class FoundationPile : Pile
+   {
+      public override bool CanAddCard(Card card)
+      {
+         if ( card.Rank == CardRank.Ace && !IsEmpty() )
+         {
+            return false;
+         }
+
+         if ( card.Rank != CardRank.Ace && IsEmpty() )
+         {
+            return false;
+         }
+        
+         if ( ! IsEmpty() )
+         {
+            if ( card.Color != _cards.Peek().Color )
+            {
+               return false;
+            }
+
+            if ( (int) card.Rank != (int) ( _cards.Peek().Rank ) + 1 )
+            {
+               return false;
+            }
+         }
+
+         return true;
+      }
+   }
+}
---
+++ SolitaireTest/FoundationPileTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solitaire;
+
+namespace SolitaireTest
+{
+   [TestClass]
+   public class FoundationPileTest
+   {
+      private FoundationPile _pile;
+
+      private readonly Card _aceOfSpades;
+      private readonly Card _twoOfSpades;
+      private readonly Card _threeOfSpades;
+      private readonly Card _aceOfDiamonds;
+      private readonly Card _twoOfDiamonds;
+      private readonly Card _threeOfDiamonds;
+
+      public FoundationPileTest()
+      {
+         _aceOfSpades = new Card( 1, CardSuit.Spades );
+         _twoOfSpades = new Card( 2, CardSuit.Spades );
+         _threeOfSpades = new Card( 3, CardSuit.Spades );
+         _aceOfDiamonds = new Card( 1, CardSuit.Diamonds );
+         _twoOfDiamonds = new Card( 2, CardSuit.Diamonds );
+         _threeOfDiamonds = new Card( 3, CardSuit.Diamonds );
+      }
+
+      [TestInitialize]
+      public void Setup()
+      {
+         _pile = new FoundationPile();
+      }
+
+      [TestMethod]
+      public void CanAddCard_PileIsEmpty_CanAddAce()
+      {
+         Assert.IsTrue( _pile.CanAddCard( _aceOfSpades ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_PileIsEmpty_CannotAddNonAce()
+      {
+         Assert.IsFalse( _pile.CanAddCard( _twoOfSpades ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_PileIsNotEmpty_CannotAddAce()
+      {
+         _pile.AddCard( _aceOfSpades );
+         Assert.IsFalse( _pile.CanAddCard( _aceOfDiamonds ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_AddingCardOfDifferentColor_CannotAdd()
+      {
+         _pile.AddCard( _aceOfSpades );
+         Assert.IsFalse( _pile.CanAddCard( _twoOfDiamonds ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_AddingCardOfSameOrLowerRank_CannotAdd()
+      {
+         _pile.AddCard( _aceOfDiamonds );
+         _pile.AddCard( _twoOfDiamonds );
+         _pile.AddCard( _threeOfDiamonds );
+         Assert.IsFalse( _pile.CanAddCard( _twoOfDiamonds ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_AddingCardOfMuchHigherRank_CannotAdd()
+      {
+         _pile.AddCard( _aceOfDiamonds );
+         Assert.IsFalse( _pile.CanAddCard( _threeOfDiamonds ) );
+      }
+
+      [TestMethod]
+      public void CanAddCard_AddingCardOfNextHigherRank_CanAdd()
+      {
+         _pile.AddCard( _aceOfDiamonds );
+         _pile.AddCard( _twoOfDiamonds );
+         Assert.IsTrue( _pile.CanAddCard( _threeOfDiamonds ) );
+      }
+
+      [TestMethod]
+      [ExpectedException( typeof( InvalidOperationException ) )]
+      public void AddCard_AddingInvalidCard_ThrowsInvalidOperationException()
+      {
+         _pile.AddCard( _twoOfSpades );
+         Assert.Fail();
+      }
+   }
+}
